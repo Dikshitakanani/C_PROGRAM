8f77e719fa66b0e510b73d491184aea8a71e3b2a
dikshita kanani <kananidixita05@gmail.com>
2023-11-05 09:34:52 +0530

    c program

---
+++ manegment/file_mw.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    FILE *fp;
+    char buff[500];
+
+    fp = fopen("file_data.txt","r");
+    
+        while(fscanf (fp,"%s",buff)!=EOF)
+        {
+            printf("%s",buff);
+        }
+        fclose(fp);
+
+       return 0;   
+}
